f59a78e4f45493e0de5f3e7f3238c06d77c32280
Varundhar_Gandham <Varundhar_Gandham@EPINHYDW013A>
2020-02-04 13:52:35 +0530

    Add project files.

---
+++ ObjectCollection/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace CollectionClasses
+{
+   
+    internal class ObjectModelCollections
+    {
+        public void collection()
+        {
+            Collection<int> collection = new Collection<int>();
+            collection.Add(1);
+            collection.Insert(0, 1);
+            collection.RemoveAt(1);
+
+            foreach (var i in collection)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        ///  class for a  read-only collection.
+        /// </summary>
+        public void readOnlyCollection()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+            ReadOnlyCollection<int> readOnly = new ReadOnlyCollection<int>(list);
+
+            foreach (var i in readOnly)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        ///class for a  readOnlyDictionary .
+        /// </summary>
+        public void readOnlyDictionary()
+        {
+            Dictionary<int, int> list = new Dictionary<int, int>();
+            list.Add(1, 100);
+            list.Add(2, 101);
+            list.Add(3, 102);
+            ReadOnlyDictionary<int, int> readOnly = new ReadOnlyDictionary<int, int>(list);
+
+            foreach (var i in readOnly)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        ///  a read-only ObservableCollection<T>.
+        /// </summary>
+        public void readOnlyObservable()
+        {
+            ObservableCollection<int> list = new ObservableCollection<int> { 1, 2, 3 };
+
+            ReadOnlyObservableCollection<int> readOnly = new ReadOnlyObservableCollection<int>(list);
+            foreach (var i in readOnly)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    internal class Collections
+    {
+        private static void Main(string[] args)
+        {
+          
+            ObjectModelCollections objectModel = new ObjectModelCollections();
+            Console.WriteLine("Collection");
+            objectModel.collection();
+            Console.WriteLine("ReadOnlyCollection");
+            objectModel.readOnlyCollection();
+            Console.WriteLine("ReadOnlyDictionary");
+            objectModel.readOnlyDictionary();
+            Console.WriteLine("ObservableCollection");
+            objectModel.readOnlyObservable();
+        }
+    }
+}
